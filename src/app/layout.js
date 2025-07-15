7fd4d60b31dc7baee7f2fd32fc68c492edbc0740
Ngô Tùng Lâm <46390185+LamTamNhu@users.noreply.github.com>
2025-07-15 15:58:22 +0700

    *fix feedback

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import FloatingButtons from "./components/floatingButtons";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
@@ -23,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`montserrat.variable} antialiased`}
       >
         <Navbar />
         {children}
