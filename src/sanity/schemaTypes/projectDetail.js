8485eecc62e2e441ef7ed7eec30019973e024ec8
Ngô Tùng Lâm <46390185+LamTamNhu@users.noreply.github.com>
2025-08-18 16:28:41 +0700

    refactor: share project detail schema

--- src/sanity/schemaTypes/projectDetail.js
+++ src/sanity/schemaTypes/projectDetail.js
@@ -1,78 +1,80 @@
+export const projectDetailFields = [
+    {
+        name: 'title',
+        title: 'Tiêu đề',
+        type: 'string',
+        validation: Rule => Rule.required()
+    },
+    {
+        name: 'shortDescription',
+        title: 'Mô tả ngắn',
+        type: 'string'
+    },
+    {
+        name: 'information',
+        title: 'Thông tin',
+        type: 'object',
+        fields: [
+            { name: 'landArea', title: 'Diện tích đất', type: 'string' },
+            { name: 'constructionArea', title: 'Diện tích xây dựng', type: 'string' },
+            { name: 'location', title: 'Địa điểm', type: 'string' },
+            { name: 'function', title: 'Công năng', type: 'string' },
+        ]
+    },
+    {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+            source: 'title',
+            maxLength: 96
+        },
+        validation: Rule => Rule.required()
+    },
+    {
+        name: 'category',
+        title: 'Danh mục',
+        type: 'string',
+        options: {
+            list: [
+                { title: 'Biệt thự', value: 'mansion' },
+                { title: 'Nhà phố', value: 'urbanHouse' },
+                { title: 'Nhà vườn', value: 'countryHouse' },
+                { title: 'Nhà tân cổ điển', value: 'neoClassicHouse' },
+                { title: 'Công trình dịch vụ', value: 'serviceBuilding' },
+            ],
+        },
+        validation: Rule => Rule.required()
+    },
+    {
+        name: 'gallery',
+        title: 'Thư viện ảnh',
+        type: 'array',
+        of: [
+            {
+                type: 'image',
+                options: { hotspot: true },
+                fields: [
+                    {
+                        name: 'alt',
+                        title: 'Alt',
+                        type: 'string',
+                        description: 'Tự động tạo từ tiêu đề nếu bỏ trống'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'body',
+        title: 'Nội dung',
+        type: 'blockContent'
+    }
+];
+
 export default {
     name: 'projectDetail',
     title: 'Dự án',
     type: 'document',
-    fields: [
-        {
-            name: 'title',
-            title: 'Tiêu đề',
-            type: 'string',
-            validation: Rule => Rule.required()
-        },
-        {
-            name: 'shortDescription',
-            title: 'Mô tả ngắn',
-            type: 'string'
-        },
-        {
-            name: 'information',
-            title: 'Thông tin',
-            type: 'object',
-            fields: [
-                { name: 'landArea', title: 'Diện tích đất', type: 'string' },
-                { name: 'constructionArea', title: 'Diện tích xây dựng', type: 'string' },
-                { name: 'location', title: 'Địa điểm', type: 'string' },
-                { name: 'function', title: 'Công năng', type: 'string' },
-            ]
-        },
-        {
-            name: 'slug',
-            title: 'Slug',
-            type: 'slug',
-            options: {
-                source: 'title',
-                maxLength: 96
-            },
-            validation: Rule => Rule.required()
-        },
-        {
-            name: 'category',
-            title: 'Danh mục',
-            type: 'string',
-            options: {
-                list: [
-                    { title: 'Biệt thự', value: 'mansion' },
-                    { title: 'Nhà phố', value: 'urbanHouse' },
-                    { title: 'Nhà vườn', value: 'countryHouse' },
-                    { title: 'Nhà tân cổ điển', value: 'neoClassicHouse' },
-                    { title: 'Công trình dịch vụ', value: 'serviceBuilding' },
-                ],
-            },
-            validation: Rule => Rule.required()
-        },
-        {
-            name: 'gallery',
-            title: 'Thư viện ảnh',
-            type: 'array',
-            of: [
-                {
-                    type: 'image',
-                    options: { hotspot: true },
-                    fields: [
-                        {
-                            name: 'alt',
-                            title: 'Alt',
-                            type: 'string',
-                            description: 'Tự động tạo từ tiêu đề nếu bỏ trống'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'body',
-            title: 'Nội dung',
-            type: 'blockContent'
-        }
-    ]
+    fields: projectDetailFields
 };
