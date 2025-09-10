c689449506299c68b94e2daf7177cd65c9570582
Ngô Tùng Lâm <46390185+LamTamNhu@users.noreply.github.com>
2025-09-10 15:40:27 +0700

    hurr durr fix this fix that

--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -45,15 +45,19 @@ export default function ProjectsGrid({projects = []}) {
                             {/* Project Details - 2 Column Grid */}
                             <div className="grid grid-cols-2 gap-2 text-sm text-gray-600 mb-6">
                                 {/* Left Column */}
-                                <div className="flex flex-col space-y-1 text-left">
-                                    <p className="truncate">Diện tích đất: {project?.landArea}</p>
-                                    <p className="truncate">Diện tích xây dựng: {project?.constructionArea}</p>
+                                <div className="text-left truncate">
+                                    <span className="font-semibold">Diện tích: </span>
+                                    <span>{project?.landArea}</span>
                                 </div>
 
                                 {/* Right Column */}
-                                <div className="flex flex-col space-y-1 text-left">
-                                    <p className="truncate">Địa điểm: {project?.location}</p>
-                                    <p className="truncate">Công năng: {project?.function}</p>
+                                <div className="text-left truncate">
+                                    <span className="font-semibold">Địa điểm: </span>
+                                    <span>{project?.location}</span>
+                                </div>
+                                <div className="text-left col-span-2">
+                                    <span className="font-semibold">Công năng: </span>
+                                    <span className="line-clamp-2 h-10">{project?.function}</span>
                                 </div>
                             </div>
 
@@ -68,7 +72,7 @@ export default function ProjectsGrid({projects = []}) {
                             <div className="mt-auto">
                                 <Link
                                     href={`/projects/${project.slug}`}
-                                    className="inline-flex text-white text-center w-40 justify-center gap-2 bg-orange-400 hover:bg-orange-300 font-medium px-4 py-2 rounded transition-colors duration-200"
+                                    className="inline-flex text-white text-center w-40 justify-center gap-2 bg-orange-400 hover:bg-orange-300 font-medium mb-2 px-4 py-2 rounded transition-colors duration-200"
                                 >
                                     Xem thêm
                                     <span className="my-auto"><MoveUpRight size={16} strokeWidth={3}/></span>
